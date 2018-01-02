66ca0b3acc8a240511d319833ec26dc2bd01dfd4
Ashok Patidar <flash.ria.developer@gmail.com>
2018-01-01 19:03:47 -0500

    added parcel bundler

---
+++ components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>Parcel Demos</h1>
+				<button>Dynamic Feature Cool Parcel</button>
+			</div>
+		);
+	}
+}
+
+export default App;
